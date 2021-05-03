62711f280b4254d27398674edd051ae492e00dae
Johan Sörngård <johan.sorngard@fysik.su.se>
2021-05-03 17:17:54 +0200

    Calls the Mandelbrot iteration for evey pixel. Now to debug!

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,19 @@ use image::RgbImage;
 //Runs the main logic of the program and returns an error to
 //main if something goes wrong
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let center_re = config.center_re;
-    let center_im = config.center_im;
+    let center_real = config.center_real;
+    let center_imag = config.center_imag;
     let aspect_ratio = config.aspect_ratio;
     let yresolution = config.resolution;
     let save_result = config.save_result;
     let xresolution = (aspect_ratio * (yresolution as f64)) as u32;
     let zoom = config.zoom;
+    let imag_distance = config.imag_distance / zoom;
+    let real_distance = aspect_ratio * imag_distance;
+    let depth = 255;
     println!(
-        "The real part of the center point is {}, and the imaginary part is {}",
-        center_re, center_im
+        "The real part of the center point is {}, and the imag part is {}",
+        center_real, center_imag
     );
     println!(
         "The aspect ratio is {}, and the resolution is {}x{}",
@@ -28,7 +31,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     }
     println!("We're gonna zoom in by a factor of {}", zoom);
 
-    render(xresolution, yresolution, center_re, center_im, zoom);
+    let img = render(
+        xresolution,
+        yresolution,
+        center_real,
+        center_imag,
+        imag_distance,
+        real_distance,
+        depth,
+    );
+
+    img.save("m.png").unwrap();
 
     //Everything finished correctly!
     Ok(())
@@ -37,14 +50,30 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub fn render(
     xresolution: u32,
     yresolution: u32,
-    _center_re: f64,
-    _center_im: f64,
-    _zoom: f64,
+    center_real: f64,
+    center_imag: f64,
+    imag_distance: f64,
+    real_distance: f64,
+    depth: u8,
 ) -> RgbImage {
-    let _img_distance = 8.0 / 3.0;
     let mut img = RgbImage::new(xresolution, yresolution);
-    for (x, y, _pixel) in img.enumerate_pixels_mut() {
-        println!("x: {}, y: {}", x, y);
+    let start_real = center_real - real_distance / 2.0;
+    let start_imag = center_imag - imag_distance / 2.0;
+    let mut intensity;
+    let mut c_real;
+    let mut c_imag;
+
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        c_real = start_real + real_distance * (x as f64) / (xresolution as f64);
+        c_imag = start_imag + imag_distance * (y as f64) / (yresolution as f64);
+        intensity = iterate(c_real, c_imag, depth as i64);
+        *pixel = image::Rgb([
+            (intensity * f64::powf(depth as f64, 1.0 - (intensity * 45.0) * 2.0)) as u8,
+            (intensity * 70.0 - (880.0 * f64::powf(intensity, 18.0))
+                + (701.0 * f64::powf(intensity, 9.0))) as u8,
+            (intensity * 80.0 + (f64::powf(intensity, 9.0) * depth as f64)
+                - (950.0 * f64::powf(intensity, 99.0))) as u8,
+        ])
     }
     return img;
 }
@@ -91,9 +120,10 @@ pub fn iterate(c_re: f64, c_im: f64, maxiterations: i64) -> f64 {
 //A struct containing the runtime specified configuration
 //of the program
 pub struct Config {
-    pub center_re: f64,
-    pub center_im: f64,
+    pub center_real: f64,
+    pub center_imag: f64,
     pub aspect_ratio: f64,
+    pub imag_distance: f64,
     pub resolution: u32,
     pub save_result: bool,
     pub zoom: f64,
@@ -105,9 +135,10 @@ impl Config {
     //struct if the arguments could be parsed correctly
     //and an error otherwise
     pub fn new() -> Result<Config, &'static str> {
-        let mut center_re = "-0.75";
-        let mut center_im = "0.0";
+        let mut center_real = "-0.75";
+        let mut center_imag = "0.0";
         let mut aspect_ratio = "1.5";
+        let imag_distance = 8.0 / 3.0;
         let mut resolution = "2160";
         let mut save_result = true;
         let mut zoom = "1";
@@ -117,22 +148,22 @@ impl Config {
             .author("Johanna Sörngård, jsorngard@gmail.com")
             .about("Renders an image of the Mandelbrot set")
             .arg(
-                Arg::new("center_re")
+                Arg::new("center_real")
                     .long("center-re")
                     .value_name("RE(CENTER)")
                     .about("the real part of the center point of the image")
                     .takes_value(true)
                     .required(false)
-                    .default_value(center_re),
+                    .default_value(center_real),
             )
             .arg(
-                Arg::new("center_im")
+                Arg::new("center_imag")
                     .long("center-im")
                     .value_name("IM(CENTER)")
-                    .about("the imaginary part of the center point of the image")
+                    .about("the imag part of the center point of the image")
                     .takes_value(true)
                     .required(false)
-                    .default_value(center_im),
+                    .default_value(center_imag),
             )
             .arg(
                 Arg::new("aspect_ratio")
@@ -174,11 +205,11 @@ impl Config {
             .get_matches();
 
         //Extract command line arguments
-        if let Some(cr) = matches.value_of("center_re") {
-            center_re = cr;
+        if let Some(cr) = matches.value_of("center_real") {
+            center_real = cr;
         }
-        if let Some(ci) = matches.value_of("center_im") {
-            center_im = ci;
+        if let Some(ci) = matches.value_of("center_imag") {
+            center_imag = ci;
         }
         if let Some(ar) = matches.value_of("aspect_ratio") {
             aspect_ratio = ar
@@ -195,12 +226,12 @@ impl Config {
 
         //Parse the inputs from strings into the appropriate
         //types
-        let center_re: f64 = match center_re.trim().parse() {
+        let center_real: f64 = match center_real.trim().parse() {
             Ok(num) => num,
             Err(_) => return Err("Could not interpret RE(CENTER) as a float"),
         };
 
-        let center_im: f64 = match center_im.trim().parse() {
+        let center_imag: f64 = match center_imag.trim().parse() {
             Ok(num) => num,
             Err(_) => return Err("Could not interpret IM(CENTER) as a float"),
         };
@@ -221,9 +252,10 @@ impl Config {
         };
 
         Ok(Config {
-            center_re,
-            center_im,
+            center_real,
+            center_imag,
             aspect_ratio,
+            imag_distance,
             resolution,
             save_result,
             zoom,
