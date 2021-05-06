9713dc4614ac5c7f07d7995318fe43c2407eda44
Johan <johan.sorngard@gmail.com>
2021-05-06 14:44:34 +0200

    Reduced use statements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use std::process;
-
-use rustybrot::Config;
-
 extern crate image;
 
 fn main() {
@@ -9,15 +5,15 @@ fn main() {
     //into the constructor of the config function. This then
     //returns a config object, or an error if the input can
     //not be parsed
-    let config = Config::new().unwrap_or_else(|err| {
+    let config = rustybrot::Config::new().unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
+        std::process::exit(1);
     });
 
     //Call the run function, and if it returns an error,
     //display it
     if let Err(e) = rustybrot::run(config) {
         eprintln!("Application encountered an error: {}", e);
-        process::exit(1);
+        std::process::exit(1);
     }
 }
