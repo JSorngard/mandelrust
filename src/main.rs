f2b9688840fa5b6a3f813d9239577c813b8ec0e4
Johan Sörngård <johan.sorngard@fysik.su.se>
2021-05-07 11:28:50 +0200

    Updated name of program to match repository name

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ fn main() {
     //into the constructor of the config function. This then
     //returns a config object, or an error if the input can
     //not be parsed
-    let config = rustybrot::Config::new().unwrap_or_else(|err| {
+    let config = mandelrust::Config::new().unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
 
     //Call the run function, and if it returns an error,
     //display it
-    if let Err(e) = rustybrot::run(config) {
+    if let Err(e) = mandelrust::run(config) {
         eprintln!("Application encountered an error: {}", e);
         std::process::exit(1);
     }
