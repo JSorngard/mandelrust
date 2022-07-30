eab50ef6df4ea3409ac714e60df866bfc0959216
Johanna <jsorngard@gmail.com>
2022-07-30 18:05:21 +0200

    Added comments

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -238,8 +238,11 @@ fn supersampled_iterate(
     escape_speed
 }
 
-///Iterates the mandelbrot function on the input number until
-///it either escapes or exceeds the maximum number of iterations.
+///Iterates the Mandelbrot function (z_(n+1) = z_n^2 + c) on the given c
+///starting with z_0 = 0 until it either escapes or the loop
+///exceeds the maximum number of iterations.
+///It then returns
+/// (maxiterations - iterations - 4.0 * sqrt(z_re_sqr + z_im_sqr)^-0.4) / maxiterations
 pub fn iterate(c_re: f64, c_im: f64, maxiterations: u64) -> f64 {
     let c_imag_sqr = c_im * c_im;
     let mag_sqr = c_re * c_re + c_imag_sqr;
