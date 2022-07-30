004da8b8e44adbb9c5f8547c98cef97207540690
Johanna <jsorngard@gmail.com>
2022-07-30 17:09:41 +0200

    Changed command line parsing

--- src/config.rs
+++ src/config.rs
@@ -1,147 +1,64 @@
-use clap::{Arg, Command};
-use std::error::Error;
+use clap::Parser;
 
-//A struct containing the runtime specified configuration
-//of the program.
-pub struct Config {
-    pub center_real: f64,
-    pub center_imag: f64,
+///Renders an image of the Mandelbrot set
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    //This struct contains the runtime specified configuration of the program.
+    #[clap(short, long, value_parser, value_name = "RE(CENTER)", default_value_t = -0.75, help= "The real part of the center point of the image")]
+    pub real_center: f64,
+
+    #[clap(
+        short,
+        long,
+        value_parser,
+        value_name = "IM(CENTER)",
+        default_value_t = 0.0,
+        help = "The imaginary part of the center point of the image"
+    )]
+    pub imag_center: f64,
+
+    #[clap(
+        short,
+        long,
+        value_parser,
+        default_value_t = 1.5,
+        help = "The aspect ratio of the image"
+    )]
     pub aspect_ratio: f64,
-    pub imag_distance: f64,
-    pub resolution: u32,
+
+    #[clap(
+        short,
+        long,
+        value_parser,
+        default_value_t = 2160,
+        help = "The number of pixels along the y-axis of the image"
+    )]
+    pub pixels: u32,
+
+    #[clap(
+        short,
+        long,
+        value_parser,
+        value_name = "SQRT(SSAA FACTOR)",
+        default_value_t = 3,
+        help = "How many samples to compute for each pixel (along one direction, the actual number of samples is the square of this number)"
+    )]
     pub ssaa: u32,
-    pub save_result: bool,
+
+    #[clap(
+        long,
+        help = "Whether to save information about the location in the complex plane that the image shows in the file name"
+    )]
     pub record_params: bool,
+
+    #[clap(
+        short,
+        long,
+        value_parser,
+        value_name = "ZOOM LEVEL",
+        default_value_t = 1.0,
+        help = "How far in to zoom on the given center point"
+    )]
     pub zoom: f64,
-}
-
-//Implementation of the Config struct.
-impl Config {
-    /*
-    Returns a Result wrapper which contains a Config
-    struct if the arguments could be parsed correctly
-    and an error otherwise.
-    */
-    pub fn new() -> Result<Config, Box<dyn Error>> {
-        let mut center_real = "-0.75";
-        let mut center_imag = "0.0";
-        let mut aspect_ratio = "1.5";
-        let imag_distance = 8.0 / 3.0;
-        let mut resolution = "2160";
-        let mut zoom = "1";
-        let mut ssaa = "3";
-
-        let matches = Command::new("mandelrust")
-            .version("1.1.0")
-            .author("Johanna Sörngård, jsorngard@gmail.com")
-            .about("Renders an image of the Mandelbrot set")
-            .arg(
-                Arg::new("center_real")
-                    .long("center-re")
-                    .value_name("RE(CENTER)")
-                    .help("The real part of the center point of the image")
-                    .takes_value(true)
-                    .required(false)
-                    .allow_hyphen_values(true)
-                    .default_value(center_real),
-            )
-            .arg(
-                Arg::new("center_imag")
-                    .long("center-im")
-                    .value_name("IM(CENTER)")
-                    .help("The imaginary part of the center point of the image")
-                    .takes_value(true)
-                    .required(false)
-                    .allow_hyphen_values(true)
-                    .default_value(center_imag),
-            )
-            .arg(
-                Arg::new("aspect_ratio")
-                    .short('r')
-                    .long("aspect-ratio")
-                    .value_name("ASPECT RATIO")
-                    .help("The aspect ratio of the image")
-                    .takes_value(true)
-                    .required(false)
-                    .default_value(aspect_ratio),
-            )
-            .arg(
-                Arg::new("resolution")
-                    .short('n')
-                    .long("number-of-points")
-                    .value_name("RESOLUTION")
-                    .help("The number of points along the imaginary axis to evaluate")
-                    .takes_value(true)
-                    .required(false)
-                    .default_value(resolution),
-            )
-            .arg(
-                Arg::new("no_save")
-                    .short('x')
-                    .help("Do not write the results to file")
-                    .takes_value(false)
-                    .required(false),
-            )
-            .arg(
-                Arg::new("zoom")
-                    .short('z')
-                    .long("zoom")
-                    .value_name("ZOOM LEVEL")
-                    .help("How far in to zoom on the given center point")
-                    .takes_value(true)
-                    .required(false)
-                    .default_value(zoom),
-            )
-            .arg(
-                Arg::new("record_params")
-                    .long("record-params")
-                    .help("Whether to save information about the location in the complex plane that the image shows in the file name")
-                    .takes_value(false)
-                    .required(false)
-            )
-            .arg(
-                Arg::new("ssaa")
-                    .short('s')
-                    .long("ssaa")
-                    .value_name("SSAA")
-                    .help("How many samples to compute for each pixel (along one direction, the actual number of samples is the square of this number)")
-                    .takes_value(true)
-                    .default_value(ssaa)
-                    .required(false),
-            )
-            .get_matches();
-
-        //Extract command line arguments
-        center_real = matches.value_of("center_real").unwrap_or(center_real);
-        center_imag = matches.value_of("center_imag").unwrap_or(center_imag);
-        aspect_ratio = matches.value_of("aspect_ratio").unwrap_or(aspect_ratio);
-        resolution = matches.value_of("resolution").unwrap_or(resolution);
-        ssaa = matches.value_of("ssaa").unwrap_or(ssaa);
-        let save_result = !matches.is_present("no_save");
-        let record_params = matches.is_present("record_params");
-        zoom = matches.value_of("zoom").unwrap_or(zoom);
-
-        //Parse the inputs from strings into the appropriate types
-        let center_real: f64 = center_real.trim().parse()?;
-        let center_imag: f64 = center_imag.trim().parse()?;
-        let aspect_ratio: f64 = aspect_ratio.trim().parse()?;
-        let resolution: u32 = resolution.trim().parse()?;
-        let ssaa: u32 = ssaa.trim().parse()?;
-        if ssaa == 0 {
-            return Err("SSAA factor must be larger than 0".into());
-        }
-        let zoom: f64 = zoom.trim().parse()?;
-
-        Ok(Config {
-            center_real,
-            center_imag,
-            aspect_ratio,
-            imag_distance,
-            resolution,
-            ssaa,
-            save_result,
-            record_params,
-            zoom,
-        })
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,44 @@
-use crate::config::Config;
-use crate::mandelbrot::{render, Frame};
 use std::error::Error;
 use std::io::{stdout, Write};
+
+use crate::{
+    config::Args,
+    mandelbrot::{render, Frame},
+};
+
+use clap::Parser;
 
 mod config;
 mod mandelbrot;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let config = Config::new()?;
-    let center_real = config.center_real;
-    let center_imag = config.center_imag;
+    let config = Args::parse();
+
+    let center_real = config.real_center;
+    let center_imag = config.imag_center;
     let aspect_ratio = config.aspect_ratio;
-    let yresolution = config.resolution;
-    let save_result = config.save_result;
+    let yresolution = config.pixels;
     let record_params = config.record_params;
     let xresolution = (aspect_ratio * (yresolution as f64)) as u32;
     let zoom = config.zoom;
-    let imag_distance = config.imag_distance / zoom;
+    let imag_distance = 8.0 / (3.0 * zoom);
     let real_distance = aspect_ratio * imag_distance;
     let ssaa = config.ssaa;
+
+    if ssaa == 0 {
+        return Err("SSAA factor must be larger than 0".into());
+    }
 
     //Output some basic information about what the program will be rendering.
     print!("---- Generating a");
     if ssaa != 1 {
-        print!(" {} times supersampled", u32::pow(ssaa, 2));
+        print!(" {} times supersampled", ssaa * ssaa);
     } else {
         print!("n");
     }
-    print!(
-        " image with a resolution of {}x{}",
-        xresolution, yresolution
-    );
+    print!(" image with a resolution of {xresolution}x{yresolution}");
     if zoom != 1.0 {
-        print!(" zoomed by a factor of {}", zoom);
+        print!(" zoomed by a factor of {zoom}");
     }
     println!(" ----");
 
@@ -41,16 +47,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     //Render the image
     let img = render(xresolution, yresolution, ssaa, draw_region)?;
 
-    if save_result {
-        print!("\rEncoding and saving image");
-        stdout().flush()?;
-        let image_name = if record_params {
-            format!("re_{center_real}_im_{center_imag}_zoom_{zoom}.png")
-        } else {
-            "m.png".to_owned()
-        };
-        img.save(image_name)?;
-    }
+    print!("\rEncoding and saving image");
+    stdout().flush()?;
+    let image_name = if record_params {
+        format!("re_{center_real}_im_{center_imag}_zoom_{zoom}.png")
+    } else {
+        "m.png".to_owned()
+    };
+    img.save(image_name)?;
     println!("\rDone                     ");
 
     //Everything finished correctly!
