10c87c5027620035dfb1765b9894f9959720e3c6
Johanna Sörngård <jsorngard@gmail.com>
2022-09-29 13:27:20 +0200

    Removed promise from docstring

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -243,7 +243,6 @@ pub fn supersampled_iterate(
 /// z_(n+1) = z_n^2 + c
 /// on the given c starting with z_0 = 0 until it either escapes
 /// or the loop exceeds the maximum number of iterations.
-/// Returns the escape speed as a number between 0 and 1.
 pub fn iterate(c_re: f64, c_im: f64, maxiterations: u32) -> f64 {
     let c_imag_sqr = c_im * c_im;
     let mag_sqr = c_re * c_re + c_imag_sqr;
