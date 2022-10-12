8af09b521b0b11bd8d66062af19fa95afda2921e
Johanna Sörngård <jsorngard@gmail.com>
2022-10-12 20:37:13 +0200

    Separate comment marker from comment text

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -49,15 +49,15 @@ pub fn render(
     render_parameters: RenderParameters,
     draw_region: Frame,
 ) -> Result<DynamicImage, Box<dyn Error>> {
-    //True if the image contains the real axis, false otherwise.
-    //If the image contains the real axis we want to mirror
-    //the result of the largest half on to the smallest.
+    // True if the image contains the real axis, false otherwise.
+    // If the image contains the real axis we want to mirror
+    // the result of the largest half on to the smallest.
     let mirror = draw_region.center_imag.abs() < draw_region.imag_distance;
 
-    //One way of doing this is to always assume we are rendering
-    //in the lower half of the complex plane. If the assumption is false
-    //we only need to flip the image vertically to get the
-    //correct result since it is symmetric under conjugation.
+    // One way of doing this is to always assume we are rendering
+    // in the lower half of the complex plane. If the assumption is false
+    // we only need to flip the image vertically to get the
+    // correct result since it is symmetric under conjugation.
     let mirror_sign = if draw_region.center_imag > 0.0 {
         -1.0
     } else {
@@ -72,15 +72,15 @@ pub fn render(
     let pixel_bytes: Vec<u8> = vec![0; xresolution * yresolution * 3];
     let pixel_ptr = Arc::new(Mutex::new(pixel_bytes));
 
-    //Make a parallel iterator over all the real values with rayon and for each
+    // Make a parallel iterator over all the real values with rayon and for each
     (0..xresolution)
         .into_par_iter()
         .progress_count(xresolution.try_into().unwrap())
         .for_each(|real| {
-            //compute the real part of c and
+            // compute the real part of c and
             let c_real =
                 start_real + draw_region.real_distance * (real as f64) / (xresolution as f64);
-            //color every pixel with that real value
+            // color every pixel with that real value
             color_column(
                 c_real,
                 render_parameters,
@@ -95,10 +95,10 @@ pub fn render(
     print!("\rRendering image");
     stdout().flush()?;
 
-    //Extract the data from the mutex
+    // Extract the data from the mutex
     let finished_pixel_data =
         (*pixel_ptr.lock().expect("the mutex was poisoned, aborting")).clone();
-    //and place it in an image buffer
+    // and place it in an image buffer
     let mut img = image::ImageBuffer::<image::Rgb<u8>, Vec<u8>>::from_vec(
         yresolution.try_into()?,
         xresolution.try_into()?,
@@ -109,18 +109,18 @@ pub fn render(
     print!("\rProcessing image");
     stdout().flush()?;
 
-    //Manipulate it to be the right side up and
+    // Manipulate it to be the right side up and
     img = image::imageops::rotate270(&img);
     if mirror_sign == -1.0 {
-        //flip in vertically if we need to due to mirroring
+        // flip in vertically if we need to due to mirroring
         image::imageops::flip_vertical_in_place(&mut img);
     }
 
-    if render_parameters.grayscale {
-        Ok(DynamicImage::ImageLuma8(image::imageops::grayscale(&img)))
+    Ok(if render_parameters.grayscale {
+        DynamicImage::ImageLuma8(image::imageops::grayscale(&img))
     } else {
-        Ok(DynamicImage::ImageRgb8(img))
-    }
+        DynamicImage::ImageRgb8(img)
+    })
 }
 
 /// Computes the colors of the pixels in a column of the image of the mandelbrot set.
@@ -143,16 +143,16 @@ fn color_column(
     let real_delta = draw_region.real_distance / (xresolution - 1) as f64;
     let imag_delta = draw_region.imag_distance / (yresolution - 1) as f64;
 
-    //Create a temporary vector to hold the results for this row of pixels
+    // Create a temporary vector to hold the results for this row of pixels
     let mut result = vec![0; yresolution * 3];
     let mut c_imag: f64;
     for y in (0..yresolution * 3).step_by(3) {
-        //Compute the imaginary part at this pixel
+        // Compute the imaginary part at this pixel
         c_imag = start_imag + draw_region.imag_distance * (y as f64) / (3.0 * yresolution as f64);
 
-        //If we have rendered all the pixels with
-        //negative imaginary part for this real
-        //part we just mirror this pixel
+        // If we have rendered all the pixels with
+        // negative imaginary part for this real
+        // part we just mirror this pixel
         if mirror && c_imag > 0.0 {
             result[y] = result[mirror_from - 3];
             result[y + 1] = result[mirror_from - 2];
@@ -181,13 +181,13 @@ fn color_column(
         }
     }
 
-    //Lock the mutex for the image pixels
+    // Lock the mutex for the image pixels
     let mut pixels = image.lock().expect("mutex was poisoned, aborting");
     for (j, i) in (xindex * yresolution * 3..yresolution * (xindex + 1) * 3).enumerate() {
-        //and copy the results into it
+        // and copy the results into it
         pixels[i] = result[j];
     }
-    //Unlock the mutex here by dropping the `MutexGuard` as it goes out of scope.
+    // Unlock the mutex here by dropping the `MutexGuard` as it goes out of scope.
 }
 
 /// Determines the color of a pixel. The color map that this function uses was taken from the python code in
@@ -238,12 +238,12 @@ pub fn supersampled_iterate(
     let mut rowoffset: f64;
     let mut esc: f64;
 
-    //Supersampling loop.
+    // Supersampling loop.
     for k in 1..i32::from(ssaa) * i32::from(ssaa) + 1 {
         coloffset = (f64::from(k % i32::from(ssaa) - 1)) * one_over_ssaa;
         rowoffset = (f64::from(k - 1) / f64::from(ssaa) - 1.0) * one_over_ssaa;
 
-        //Compute escape speed of point.
+        // Compute escape speed of point.
         esc = iterate(
             c_real + rowoffset * real_delta,
             c_imag + coloffset * imag_delta,
@@ -252,10 +252,10 @@ pub fn supersampled_iterate(
         escape_speed += esc;
         samples += 1;
 
-        //If we are far from the fractal we do not need to supersample.
+        // If we are far from the fractal we do not need to supersample.
         if esc > 0.9 {
-            //Uncomment the next line to only show supersampling region as non-black.
-            //escape_speed = 0.0;
+            // Uncomment the next line to only show supersampling region as non-black.
+            // escape_speed = 0.0;
             break;
         }
     }
@@ -264,9 +264,9 @@ pub fn supersampled_iterate(
 }
 
 /// Iterates the Mandelbrot function
-/// 
+///
 /// z_(n+1) = z_n^2 + c
-/// 
+///
 /// on the given c starting with z_0 = c until it either escapes
 /// or the loop exceeds the maximum number of iterations.
 /// Returns the escape speed of the point as a number between 0 and 1.
@@ -274,7 +274,7 @@ pub fn iterate(c_re: f64, c_im: f64, maxiterations: u32) -> f64 {
     let c_imag_sqr = c_im * c_im;
     let mag_sqr = c_re * c_re + c_imag_sqr;
 
-    //Check whether the point is within the main cardioid or period 2 bulb.
+    // Check whether the point is within the main cardioid or period 2 bulb.
     if (c_re + 1.0) * (c_re + 1.0) + c_imag_sqr <= 0.0625
         || mag_sqr * (8.0 * mag_sqr - 3.0) <= 0.09375 - c_re
     {
@@ -286,12 +286,12 @@ pub fn iterate(c_re: f64, c_im: f64, maxiterations: u32) -> f64 {
     let mut z_re_sqr = mag_sqr - c_imag_sqr;
     let mut z_im_sqr = c_imag_sqr;
 
-    //We have effectively performed one iteration of the function
-    //by setting the starting values as above.
+    // We have effectively performed one iteration of the function
+    // by setting the starting values as above.
     let mut iterations = 1;
 
-    //Iterates the mandelbrot function.
-    //This loop uses only 3 multiplications, which is the minimum.
+    // Iterates the mandelbrot function.
+    // This loop uses only 3 multiplications, which is the minimum.
     while iterations < maxiterations && z_re_sqr + z_im_sqr <= 36.0 {
         z_im *= z_re;
         z_im += z_im;
@@ -305,12 +305,12 @@ pub fn iterate(c_re: f64, c_im: f64, maxiterations: u32) -> f64 {
     if iterations == maxiterations {
         0.0
     } else {
-        //This takes the escape distance, |z|, and the number of iterations to escape
-        //and maps it smoothly to the range [0, 1]. This reduces color banding.
+        // This takes the escape distance, |z|, and the number of iterations to escape
+        // and maps it smoothly to the range [0, 1]. This reduces color banding.
         (f64::from(maxiterations - iterations) - 4.0 * (z_re_sqr + z_im_sqr).sqrt().powf(-0.4))
             / f64::from(maxiterations)
-        //-4*x^(-0.4) is an approximation of
-        //ln(ln(x))/ln(2) - 2.8
-        //that works well in the range 6 < x < 36, which is what's relevant for this implementation.
+        // -4*x^(-0.4) is an approximation of
+        // ln(ln(x))/ln(2) - 2.8
+        // that works well in the range 6 < x < 36, which is what's relevant for this implementation.
     }
 }
