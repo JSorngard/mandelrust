813f53f2e1bc60155c009f73ba1468bc1602b605
Johanna Sörngård <jsorngard@gmail.com>
2023-01-16 17:47:04 +0100

    Viewer now initializes by rendering an image of the set

--- mandelview/src/main.rs
+++ mandelview/src/main.rs
@@ -1,48 +1,81 @@
 use iced::{
-    widget::{button, column, text},
-    Alignment, Element, Sandbox, Settings,
+    executor,
+    widget::{image::Handle, Container, Image},
+    Application, Command, Element, Length, Settings, Theme,
 };
 
-fn main() -> iced::Result {
-    Counter::run(Settings::default())
+use mandellib::{render, Frame, RenderParameters};
+
+fn main() {
+    MandelViewer::run(Settings::default()).unwrap();
 }
 
-struct Counter {
-    value: i32,
-}
+struct MandelViewer;
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    Increment,
-    Decrement,
-}
+impl Application for MandelViewer {
+    type Executor = executor::Default;
+    type Message = ();
+    type Flags = ();
+    type Theme = Theme;
 
-impl Sandbox for Counter {
-    type Message = Message;
-
-    fn new() -> Self {
-        Self { value: 0 }
+    fn new(_flags: ()) -> (MandelViewer, Command<Self::Message>) {
+        (MandelViewer, Command::none())
     }
 
     fn title(&self) -> String {
-        "Counter test".into()
+        String::from("Mandelviewer")
     }
 
-    fn update(&mut self, message: Self::Message) {
-        match message {
-            Message::Increment => self.value += 1,
-            Message::Decrement => self.value -= 1,
-        }
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
     }
 
     fn view(&self) -> Element<Self::Message> {
-        column![
-            button("+").on_press(Message::Increment),
-            text(self.value).size(50),
-            button("-").on_press(Message::Decrement),
-        ]
-        .padding(20)
-        .align_items(Alignment::Center)
-        .into()
+        const X_RES: u32 = 1920;
+        const Y_RES: u32 = 1080;
+
+        let params = RenderParameters::new(
+            X_RES.try_into().unwrap(),
+            Y_RES.try_into().unwrap(),
+            255.try_into().unwrap(),
+            3.try_into().unwrap(),
+            false,
+        )
+        .unwrap();
+        let l = 8.0 / 3.0;
+        let frame = Frame::new(-0.75, 0.0, f64::from(X_RES) / f64::from(Y_RES) * l, l);
+        let image_handle = Handle::from_pixels(
+            X_RES,
+            Y_RES,
+            rgb8_to_rgba8(render(params, frame, false).into_bytes()),
+        );
+
+        // let image_handle = match f(1920, 1080) {
+        //     DynamicImage::ImageRgb8(img) => {
+        //         Handle::from_pixels(1920, 1080, rgb8_to_rgba8(img.into_raw()))
+        //     }
+        //     DynamicImage::ImageLuma8(img) => {
+        //         Handle::from_pixels(1920, 1080, rgb8_to_rgba8(img.into_raw()))
+        //     }
+        //     _ => Handle::from_memory(PRERENDERED),
+        // };
+
+        let image = Image::new(image_handle)
+            .width(Length::Fill)
+            .height(Length::Fill);
+
+        Container::new(image)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
     }
+}
+
+fn rgb8_to_rgba8(pixel_bytes: Vec<u8>) -> Vec<u8> {
+    pixel_bytes
+        .chunks_exact(3)
+        .flat_map(|rgb| [rgb[0], rgb[1], rgb[2], u8::MAX])
+        .collect()
 }
