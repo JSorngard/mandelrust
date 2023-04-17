9b0ff41f82832c45f7785c5b0f651d2207bb8c75
Johanna Sörngård <jsorngard@gmail.com>
2023-04-17 10:41:10 +0200

    remove unused lazy_static dependency and unused code

--- color-space/src/lib.rs
+++ color-space/src/lib.rs
@@ -1,7 +1,6 @@
 use core::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 
 use image::{ColorType, Luma, Rgb, Rgba};
-use lazy_static::lazy_static;
 
 /// Determines the color of a pixel in linear RGB color space.
 /// The color map that this function uses was taken from the python code in
@@ -34,29 +33,6 @@ pub fn palette(escape_speed: f64) -> LinearRGB {
         ]
         .map(srgb_to_linear_rgb),
     )
-}
-
-lazy_static! {
-    pub static ref SRGB_TO_LINEAR: Vec<f64> = (0..=u8::MAX)
-        .map(|c_srgb| {
-            let c_linear = f64::from(c_srgb) / 255.0;
-            if c_linear <= 0.04045 {
-                c_linear / 12.92
-            } else {
-                ((c_linear + 0.055) / 1.055).powf(2.4)
-            }
-        })
-        .collect();
-    pub static ref LINEAR_TO_SRGB: Vec<f64> = (0..=u8::MAX)
-        .map(|c_linear| {
-            let c_srgb = f64::from(c_linear) / 255.0;
-            if c_srgb <= 0.0031308 {
-                12.92 * c_srgb
-            } else {
-                1.055 * c_srgb.powf(1.0 / 2.4) - 0.055
-            }
-        })
-        .collect();
 }
 
 /// An RGB triplet whose underlying data is not in an sRGB format,
@@ -310,34 +286,6 @@ impl TryFrom<ColorType> for SupportedColorType {
             ColorType::Rgb32F => Err(UnsupportedColorTypeError::Rgb32F),
             ColorType::Rgba32F => Err(UnsupportedColorTypeError::Rgba32F),
             _ => Err(UnsupportedColorTypeError::Unknown),
-        }
-    }
-}
-
-#[cfg(test)]
-mod test_color_space {
-    use super::*;
-    use approx::assert_relative_eq;
-    use image::Rgb;
-    use itertools::Itertools;
-
-    #[test]
-    fn check_reversibillity_of_colorspace_conversions() {
-        let norm = f64::from(u8::MAX);
-        for (r, (g, b)) in
-            (0..u8::MAX).cartesian_product((0..=u8::MAX).cartesian_product(0..=u8::MAX))
-        {
-            let rf = f64::from(r) / norm;
-            let gf = f64::from(g) / norm;
-            let bf = f64::from(b) / norm;
-
-            let linear_rgb = LinearRGB::new(rf, gf, bf);
-            let srgb: Rgb<f64> = linear_rgb.into();
-            let after_conversions: LinearRGB = srgb.into();
-
-            assert_relative_eq!(linear_rgb.r, after_conversions.r);
-            assert_relative_eq!(linear_rgb.g, after_conversions.g);
-            assert_relative_eq!(linear_rgb.b, after_conversions.b);
         }
     }
 }
