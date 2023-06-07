3b88c7fddb22b1fc3c3350917b770ce93abdd418
Johanna Sörngård <jsorngard@gmail.com>
2023-06-07 11:19:20 +0200

    rustfmt

--- color-space/benches/colorbenches.rs
+++ color-space/benches/colorbenches.rs
@@ -1,5 +1,5 @@
-use criterion::{criterion_group, criterion_main, Criterion, Throughput, Bencher};
 use color_space::{palette, LinearRGB};
+use criterion::{criterion_group, criterion_main, Bencher, Criterion, Throughput};
 use image::Rgb;
 
 fn bench_color_space(c: &mut Criterion) {
@@ -8,17 +8,35 @@ fn bench_color_space(c: &mut Criterion) {
     group.throughput(Throughput::Elements(MAG.into()));
     let speeds: Vec<f64> = (0..MAG).map(|v| f64::from(v) / f64::from(MAG)).collect();
     let speeds_ref: &[f64] = &speeds;
-    group.bench_with_input("palette evaluation", speeds_ref, |b: &mut Bencher, speeds: &[f64]| {
-        b.iter(|| speeds.iter().map(|s| std::hint::black_box(palette(*s))).collect::<Vec<_>>())
-    });
+    group.bench_with_input(
+        "palette evaluation",
+        speeds_ref,
+        |b: &mut Bencher, speeds: &[f64]| {
+            b.iter(|| {
+                speeds
+                    .iter()
+                    .map(|s| std::hint::black_box(palette(*s)))
+                    .collect::<Vec<_>>()
+            })
+        },
+    );
     drop(speeds_ref);
-    
+
     let colors: Vec<LinearRGB> = speeds.into_iter().map(palette).collect();
     let colors_ref: &[LinearRGB] = &colors;
-    group.bench_with_input("linear<f64> to srgb<u8> conversion", colors_ref, |b: &mut Bencher, colors: &[LinearRGB]| {
-        b.iter(|| colors.iter().map(|color| std::hint::black_box(Rgb::<u8>::from(*color))).collect::<Vec<_>>())
-    });
-    
+    group.bench_with_input(
+        "linear<f64> to srgb<u8> conversion",
+        colors_ref,
+        |b: &mut Bencher, colors: &[LinearRGB]| {
+            b.iter(|| {
+                colors
+                    .iter()
+                    .map(|color| std::hint::black_box(Rgb::<u8>::from(*color)))
+                    .collect::<Vec<_>>()
+            })
+        },
+    );
+
     group.finish();
 }
 
